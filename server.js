89243932e084e613b59f817d553a248a2a6fa973
scott <scott.a.benson189@gmail.com>
2021-03-02 18:22:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 
-var server_port = process.env.PORT || process.env.YOUR_PORT || 8080;
-
+var server_port = process.env.PORT || process.env.YOUR_PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
 console.log("Database_URL", process.env.DATABASE_URL);
 
-app.listen(server_port, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
+    console.log('Listening on host %d', server_host);
 });
