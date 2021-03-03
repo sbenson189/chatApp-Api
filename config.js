22f4dccc39f075a17c51e0558923ec0e047748cb
scott <scott.a.benson189@gmail.com>
2021-03-02 18:53:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require("dotenv").config()
 let DB_URI = `postgresql://`
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/chat_db?ssl=true`
+  DB_URI = `${DB_URI}/chat_db?ssl=false`
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/chat_db?ssl=true`
+  DB_URI = process.env.DATABASE_URL || `${DB_URI}/chat_db?ssl=false`
 }
 
 // Would typically not show key
