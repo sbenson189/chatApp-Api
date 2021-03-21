ad930b2425135cc2f50d8d8a768cf67486391df2
scott <scott.a.benson189@gmail.com>
2021-03-20 19:53:43 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = new express.Router()
 const ExpressError = require('../expressError')
-const db = require('../db')
+const { BCRYPT_WORK_FACTOR, SECRET_KEY, db } = require('../db')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require('../config')
+// const { BCRYPT_WORK_FACTOR, SECRET_KEY, } = require('../config')
 const fileUpload = require('express-fileupload')
 
 // const { ensureLoggedIn } = require('../middleware/auth')
