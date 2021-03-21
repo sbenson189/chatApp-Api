4fb871bc542faf3f88d1f6b21dbc6f40af26a42c
scott <scott.a.benson189@gmail.com>
2021-03-20 19:01:39 -0500

    DB_URI/URL changed

--- db.js
+++ db.js
@@ -5,13 +5,14 @@ let DB_URI = `postgresql://`
 if (process.env.NODE_ENV === "test") {
   DB_URI = `${DB_URI}/chat_db`
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/chat_db`
+  // DB_URI = process.env.DATABASE_URL || `${DB_URI}/chat_db`
+  DB_URI = `${DB_URI}/chat_db`
 }
 
 let db = new Client({
-    // connectionString: DB_URI, // need to give new Client instance a connection point (which we established above)
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    // connectionString: DB_URI, 
+    connectionString: process.env.DATABASE_URL !== undefinded ? process.env.DATABASE_URL : DB_URI,
+    // ssl: false
 })
 
 db.connect() // db is name of variable established above, we are connecting to it.
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-var server_port = process.env.PORT || process.env.YOUR_PORT || 80;
+var server_port = process.env.PORT || process.env.YOUR_PORT || 5000;
 console.log("Database_URL", process.env.DATABASE_URL);
 
 app.listen(server_port, function() {
